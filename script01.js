2858b5ddde84e5be16b1c0c28c737d83027234fb
Otavio Pavoni <otaviohabbiano123@gmail.com>
2021-09-07 09:08:34 -0300

    Quase tudo feito, só deixar o código um pouco mais limpo / Almost everything done.

--- script01.js
+++ script01.js
@@ -1,7 +1,9 @@
-var img = document.getElementsByName('img')
+//var img = document.getElementsByName('img')
 var lista = document.getElementById('Lista')
 let numal = Math.floor(Math.random(1, 100) * 100)
 var p1 = document.getElementById('p1')
+var matriz1 = []
+window.alert(numal)
 
 function aleatorio(){
     numal = Math.floor(Math.random(1, 100) * 100)
@@ -11,39 +13,50 @@ function rodar(){
     let num1 = document.getElementById('num1')
     let n1 = Number(num1.value)
 
-    if (n1 == 0){
-        p1.innerHTML = `Por favor, digite um número!`
+    if (n1 > 100 || n1 < 1){
+
+        p1.innerHTML = `Por favor, digite um número entre 1 e 100`
+        window.alert('Por favor, digite um número entre 1 e 100')
+
     } else if(n1 > numal){
+
         p1.innerHTML = `O número que você digitou é MAIOR que o número alvo.`
+
         let item = document.createElement('option')
         item.text = `Adicionado ${n1} - MAIOR`
         lista.appendChild(item)
+        matriz1.push(n1)
 
     } else if(n1 < numal){
+
         p1.innerHTML = `O número que você digitou é MENOR que o número alvo.`
+
         let item = document.createElement('option')
         item.text = `Adicionado ${n1} - MENOR`
         lista.appendChild(item)
+        matriz1.push(n1)
 
     } else if(n1 == numal){
-        p1.innerHTML = `Parabéns, você ganhou! O número alvo é ${numal}`
+
+        p1.innerHTML = `Parabéns, você ganhou! O número alvo é ${numal}.`
+
         let item = document.createElement('option')
         item.text = `Adicionado ${n1} - ALVO`
         lista.appendChild(item)
+        matriz1.push(n1)
 
-        p1.innerHTML += `Caso queira jogar novamente, clique em "Resetar"`
+        p1.innerHTML += `<br>Caso queira jogar novamente, clique em "Resetar" (não esqueça de tirar o número atual da caixa de número)<br>`
+        
+        p1.innerHTML += `<br>------------------------------------------<br>`
+
+        p1.innerHTML += `<br>Você precisou digitar ${matriz1.length} números para descobrir o alvo.`
     }
 }
 
-function jogo(){
-    rodar()
-}
-
 function resetar(){
-    let num1 = document.getElementById('num1')
-    num1.length = 0
+    matriz1 = []
     p1.innerHTML = ''
     lista.length = 0
     aleatorio()
-    jogo()
+    rodar()
 }
