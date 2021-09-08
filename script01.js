fcc041c45ae7e0a945c7ee972fc79cb0d71daca9
Otavio Pavoni <otaviohabbiano123@gmail.com>
2021-09-08 17:17:59 -0300

    Images Update - Atualização de Imagens

--- script01.js
+++ script01.js
@@ -2,6 +2,7 @@ var lista = document.getElementById('Lista')
 let numal = Math.floor(Math.random(1, 100) * 100)
 var p1 = document.getElementById('p1')
 var matriz1 = []
+var img = document.getElementById('img')
 
 function aleatorio(){
     numal = Math.floor(Math.random(1, 100) * 100)
@@ -13,6 +14,7 @@ function rodar(){
 
     if (n1 > 100 || n1 < 1){
 
+        img.src = "imagens/error-200.png"
         p1.innerHTML = `Por favor, digite um número entre 1 e 100`
         window.alert('Por favor, digite um número entre 1 e 100')
 
@@ -20,15 +22,18 @@ function rodar(){
 
         p1.innerHTML = `O número que você digitou é MAIOR que o número alvo.`
 
+        img.src = 'imagens/maior-200.png'
         let item = document.createElement('option')
         item.text = `Adicionado ${n1} - MAIOR`
         lista.appendChild(item)
         matriz1.push(n1)
 
+
     } else if(n1 < numal){
 
         p1.innerHTML = `O número que você digitou é MENOR que o número alvo.`
 
+        img.src = 'imagens/menor-200.png'
         let item = document.createElement('option')
         item.text = `Adicionado ${n1} - MENOR`
         lista.appendChild(item)
@@ -38,6 +43,7 @@ function rodar(){
 
         p1.innerHTML = `Parabéns, você ganhou! O número alvo é ${numal}.`
 
+        img.src = 'imagens/certo-200.png'
         let item = document.createElement('option')
         item.text = `Adicionado ${n1} - ALVO`
         lista.appendChild(item)
